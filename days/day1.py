26820f2f64f3febb1674c88bdf013be72fff16ab
Langlois Clément <langlois.clement@gmail.com>
2024-12-01 14:43:42 +0100

    Day 1 part 2

--- days/day1.py
+++ days/day1.py
@@ -1,4 +1,5 @@
 from pkg_resources import resource_stream
+from collections import Counter
 
 def part1():
     listA, listB = [], []
@@ -13,4 +14,18 @@ def part1():
     r = 0
     for i in range(len(listA)):
         r += abs(listA[i] - listB[i])
+    return r
+
+def part2():
+    listA, listB = [], []
+    with resource_stream("input", "D1.txt") as textInput:
+        for line in textInput.readlines():
+            line = line.decode().strip()
+            numbers = line.split()
+            listA.append(int(numbers[0]))
+            listB.append(int(numbers[1]))
+    count=Counter(listB) 
+    r=0
+    for n in listA:
+        r+=n*count[n]
     return r
