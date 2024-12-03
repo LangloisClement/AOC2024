f01bcc23f07c45927ea372cfa4658fce35857632
Langlois Clément <langlois.clement@gmail.com>
2024-12-03 11:39:06 +0100

    Day 3 part 2

--- days/day3.py
+++ days/day3.py
@@ -15,3 +15,22 @@ def part1():
             ret += reduce((lambda x, y: int(x) * int(y)), t)
 
     return ret
+
+def part2():
+    ret = 0
+    instruct = []
+    with resource_stream("input", "D3.txt") as textInput:
+        for line in textInput.readlines():
+            line = line.decode().strip()
+            instruct.append(findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", line))
+    enabled=True
+    for i in instruct:
+        for s in i:
+            if s=="do()":
+                enabled=True
+            elif s=="don't()":
+                enabled=False
+            else:
+                s=findall(r"mul\((\d{1,3}),(\d{1,3})\)", s)
+                ret += reduce((lambda x, y: int(x) * int(y)), s[0]) if enabled else 0
+    return ret
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Advent Of Code 2023
 from days import day1, day2, day3
 print("Merry Advent of Code 2024!")
-print(day3.part1())
+print(day3.part2())
 
