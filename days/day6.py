d21547d46db196fd559630b868c8cd07d56e1ff5
Langlois Clément <langlois.clement@gmail.com>
2024-12-06 17:30:02 +0100

    Day 6 part 2 (bit slow)

--- days/day6.py
+++ days/day6.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import copy
 from functools import reduce
 from pkg_resources import resource_stream
 
@@ -47,3 +48,49 @@ def part1():
                 plan[position[0]][position[1]] = "X"
                 position = ahead
     return reduce((lambda a, b: a + b), [Counter(s)["X"] for s in plan])
+
+
+def is_loop(plan, init):
+    position = init
+    facing = 0
+    visited = [(position, facing)]
+    is_out = False
+    while not is_out:
+        ahead = position.copy()
+        match DIRECTION[facing]:
+            case "UP":
+                ahead[0] -= 1
+            case "RIGHT":
+                ahead[1] += 1
+            case "DOWN":
+                ahead[0] += 1
+            case "LEFT":
+                ahead[1] -= 1
+            case _:
+                raise Exception
+        if (ahead, facing) in visited:
+            return True
+        visited.append((position, facing))
+        if ahead[0] < 0 or ahead[0] >= L or ahead[1] < 0 or ahead[1] >= C:
+            plan[position[0]][position[1]] = "X"
+            is_out = True
+            continue
+        match plan[ahead[0]][ahead[1]]:
+            case "#":
+                facing = (facing + 1) % 4
+            case _:
+                plan[position[0]][position[1]] = "X"
+                position = ahead
+    return False
+
+
+def part2():
+    position = get_pos()
+    ret = 0
+    for i in range(L):
+        for j in range(C):
+            if plan[i][j] not in "^#":
+                plan_obstacle = copy.deepcopy(plan)
+                plan_obstacle[i][j] = "#"
+                ret += 1 if is_loop(plan_obstacle, position.copy()) else 0
+    return ret
--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 from days import day1, day2, day3, day4, day5, day6
 
 print("Merry Advent of Code 2024!")
-print(day6.part1())
+print(day6.part2())
